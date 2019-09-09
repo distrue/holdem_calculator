c346a9b21e2559d70f5067a79c7825bff8b7119c
Y.Jung <gilsang9808@gmail.com>
2019-09-09 23:03:19 +0900

    Fixedcard click 문제 해결

--- frontend/dispatcher/refresh.ts
+++ frontend/dispatcher/refresh.ts
@@ -1,6 +1,6 @@
 import {shareChange} from './share';
 export const refresh = (newPlayer, labelStore, playerStore, blockStore, shareStore) => {
-    labelStore.now = undefined;
+    labelStore.now = 1;
     playerStore.now = newPlayer;
     blockStore.constructor({labelStore:labelStore, newPlayer:newPlayer});
     shareChange(shareStore, playerStore, labelStore, blockStore);
