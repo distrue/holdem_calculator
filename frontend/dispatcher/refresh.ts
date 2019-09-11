c0e5e53a341947aa0b43f64e1b24fa4f52844beb
cappitio <IOcappit>
2019-09-11 22:18:52 +0900

    bug fixed: labelNum error

--- frontend/dispatcher/refresh.ts
+++ frontend/dispatcher/refresh.ts
@@ -1,7 +1,7 @@
 import {shareChange} from './share';
 export const refresh = (newPlayer, labelStore, playerStore, blockStore, shareStore) => {
     playerStore.now = newPlayer;
-    labelStore.now = 1;
+    labelStore.now = 1 + 12 * (playerStore.now - 1);
     blockStore.constructor({labelStore:labelStore, newPlayer:newPlayer});
     shareChange(shareStore, playerStore, labelStore, blockStore);
 }
