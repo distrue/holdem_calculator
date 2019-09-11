2106f7db58c5e6a4d79d5419e39ed57044ba870b
cappitio <IOcappit>
2019-09-11 19:16:36 +0900

    bug fixed: all block go to player 1 label

--- frontend/dispatcher/refresh.ts
+++ frontend/dispatcher/refresh.ts
@@ -1,6 +1,6 @@
 import {shareChange} from './share';
 export const refresh = (newPlayer, labelStore, playerStore, blockStore, shareStore) => {
-    labelStore.now = 1;
+    labelStore.now = 12 * (newPlayer - 1) + 1;
     playerStore.now = newPlayer;
     blockStore.constructor({labelStore:labelStore, newPlayer:newPlayer});
     shareChange(shareStore, playerStore, labelStore, blockStore);
